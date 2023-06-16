0ce35afc96edc79f64c98bf510d52104ef7caa59
Hamza Abonaser <2169000@talnet.nl>
2023-06-16 13:31:49 +0200

    1

---
+++ opdracht4.php
@@ -0,0 +1,41 @@
+<?php
+// for ($i = 0; $i <= 50; $i++) {
+//        echo $i . " ";
+//    }
+
+//    $klasgenoten = [
+//        "Jan",
+//        "Piet",
+//        "Klaas",
+//        "Marieke",
+//        "Eva",
+//        "Sander",
+//        "Lisa",
+//        "Johan",
+//        "Emma",
+//        "Mira"
+//    ];
+//  foreach ($klasgenoten as $naam) {
+//     echo $naam . "<br>";
+// }
+//  $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+//  $aantalMaanden = count($maanden);
+//  for ($i = 0; $i < $aantalMaanden; $i++) {
+//     echo 'Maand ' . ($i + 1) . ' is ' . $maanden[$i] . '.<br />';
+// }
+   
+// $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+// $maandNummer = 1;
+// foreach ($maanden as $maand) {
+//     echo 'Maand ' . $maandNummer . ' is ' . $maand . '.<br />';
+//     $maandNummer++;
+// }
+   
+   
+   
+
+
+
+
+
+?>
